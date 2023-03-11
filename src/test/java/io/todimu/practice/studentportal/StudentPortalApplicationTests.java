1a328d189186277f3c0b4a25f5f0360b232041f2
todimuisewon <todimuisewon@gmail.com>
2023-03-11 19:49:40 +0100

    initial commit

---
+++ src/test/java/io/todimu/practice/studentportal/StudentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.todimu.practice.studentportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
