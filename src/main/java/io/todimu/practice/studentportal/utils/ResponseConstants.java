b6b33ace08d33dade61ade9de2cec1f68fbf5044
todimuisewon <todimuisewon@gmail.com>
2023-05-29 20:12:51 +0100

    updated code

--- src/main/java/io/todimu/practice/studentportal/utils/ResponseConstants.java
+++ src/main/java/io/todimu/practice/studentportal/utils/ResponseConstants.java
@@ -2,5 +2,6 @@ package io.todimu.practice.studentportal.utils;
 
 public class ResponseConstants {
 
-    public static final String SUCCESS = "Successful action";
+    public static final String SUCCESS = "successful";
+    public static final String FAILED = "failed";
 }
