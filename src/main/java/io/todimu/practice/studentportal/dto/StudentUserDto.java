4a3c7274d13202949db0405bbc15a30cec5eeb30
todimuisewon <todimuisewon@gmail.com>
2023-05-29 22:50:16 +0100

    added activation key spring components

--- src/main/java/io/todimu/practice/studentportal/dto/StudentUserDto.java
+++ src/main/java/io/todimu/practice/studentportal/dto/StudentUserDto.java
@@ -1,5 +1,6 @@
 package io.todimu.practice.studentportal.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudentUserDto {
 
     private UUID userId;
