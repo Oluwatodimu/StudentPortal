6424858f3f202a2ad3cad255af9ebfc3f5fe368d
todimuisewon <todimuisewon@gmail.com>
2023-05-29 05:30:47 +0100

    added password encoder config to use BCryptEncoder

--- src/main/java/io/todimu/practice/studentportal/config/SecurityConfig.java
+++ src/main/java/io/todimu/practice/studentportal/config/SecurityConfig.java
@@ -1,10 +1,12 @@
-package io.todimu.practice.studentportal.security;
+package io.todimu.practice.studentportal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,4 +21,10 @@ public class SecurityConfig {
                 .requestMatchers("/api/v1/student/**").permitAll();
         return httpSecurity.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
