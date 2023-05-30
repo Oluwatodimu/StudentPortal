4c3649791f873bbdd8907bb3bbc3934c2753b6ca
todimuisewon <todimuisewon@gmail.com>
2023-05-30 05:01:49 +0100

    added token provider for creating and validating jwt tokens

--- src/main/java/io/todimu/practice/studentportal/security/jwt/TokenProvider.java
+++ src/main/java/io/todimu/practice/studentportal/security/jwt/TokenProvider.java
@@ -4,8 +4,10 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.todimu.practice.studentportal.utils.AuthoritiesConstants;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -26,7 +28,7 @@ public class TokenProvider {
                     .claim("username", authentication.getName())
                     .claim("authorities", populateAuthorities(authentication.getAuthorities()))
                     .setIssuedAt(new Date())
-                    .setExpiration(new Date((new Date()).getTime() + 30000000))
+                    .setExpiration(new Date((new Date()).getTime() + 120000))
                     .signWith(secretKey).
                     compact();
 
@@ -38,16 +40,30 @@ public class TokenProvider {
 
     public boolean validateToken(String jwtToken) {
         try {
-            SecretKey key = Keys.hmacShaKeyFor(AuthoritiesConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
-            Claims claims = Jwts.parserBuilder().setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(jwtToken)
-                    .getBody();
+            Claims claims = getClaimsFromToken(jwtToken);
             return true;
         } catch (RuntimeException exception) {
             System.err.println("token validation failed");
             return false;
         }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaimsFromToken(token);
+        String username = (String) claims.get("username");
+        String authoritiesString = (String) claims.get("authorities");
+
+        return new UsernamePasswordAuthenticationToken(username,
+                null,
+                AuthorityUtils.commaSeparatedStringToAuthorityList(authoritiesString));
+    }
+
+    private Claims getClaimsFromToken(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(AuthoritiesConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
+        return Jwts.parserBuilder().setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     private String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
