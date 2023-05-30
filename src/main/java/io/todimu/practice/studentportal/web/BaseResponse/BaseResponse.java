0a524594e653560383e0894c3e66068db81b1803
todimuisewon <todimuisewon@gmail.com>
2023-05-30 21:56:59 +0100

    added exception handlers to the code

--- src/main/java/io/todimu/practice/studentportal/web/BaseResponse/BaseResponse.java
+++ src/main/java/io/todimu/practice/studentportal/web/BaseResponse/BaseResponse.java
@@ -1,10 +1,12 @@
 package io.todimu.practice.studentportal.web.BaseResponse;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseResponse {
 
     private Object data;
