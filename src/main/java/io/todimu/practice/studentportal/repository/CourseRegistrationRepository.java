ae176d40984697da2c99fe2615d91c1935f3058d
todimuisewon <todimuisewon@gmail.com>
2023-06-05 07:39:56 +0100

    fixed code

--- src/main/java/io/todimu/practice/studentportal/repository/CourseRegistrationRepository.java
+++ src/main/java/io/todimu/practice/studentportal/repository/CourseRegistrationRepository.java
@@ -1,11 +1,15 @@
 package io.todimu.practice.studentportal.repository;
 
 import io.todimu.practice.studentportal.model.CourseRegistration;
+import io.todimu.practice.studentportal.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CourseRegistrationRepository extends JpaRepository<CourseRegistration, UUID> {
+
+    List<CourseRegistration> findAllByStudent(Student student);
 }
