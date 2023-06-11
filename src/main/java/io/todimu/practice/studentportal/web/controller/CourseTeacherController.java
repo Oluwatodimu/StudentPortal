15c47703779a7bf1f4f2a3d797baf786a2262908
todimuisewon <todimuisewon@gmail.com>
2023-06-11 21:15:35 +0100

    renamed directory

--- src/main/java/io/todimu/practice/studentportal/web/controller/CourseTeacherController.java
+++ src/main/java/io/todimu/practice/studentportal/web/controller/CourseTeacherController.java
@@ -1,10 +1,9 @@
 package io.todimu.practice.studentportal.web.controller;
 
 import io.todimu.practice.studentportal.dto.request.AssignTeacherToCourseRequest;
-import io.todimu.practice.studentportal.model.CourseTeacher;
 import io.todimu.practice.studentportal.service.CourseTeacherService;
 import io.todimu.practice.studentportal.utils.ResponseConstants;
-import io.todimu.practice.studentportal.web.BaseResponse.BaseResponse;
+import io.todimu.practice.studentportal.web.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
