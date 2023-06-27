34e21ca6a61a156986be8869d26992cb656dcd36
todimuisewon <todimuisewon@gmail.com>
2023-06-27 21:11:48 +0100

    changed file location

--- src/main/java/io/todimu/practice/studentportal/annotation/RateLimited.java
+++ src/main/java/io/todimu/practice/studentportal/annotation/RateLimited.java
@@ -5,13 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RateLimited {
 
 }
 
-// todo use AOP to check for rate limiting
 // todo enable logging and monitoring with Treblle
 // todo deploy application on render
 // todo write github actions
