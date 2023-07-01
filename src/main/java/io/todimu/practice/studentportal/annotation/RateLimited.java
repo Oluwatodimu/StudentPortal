43bcf89e610d7a89f298f1bd8ead36061521da0e
todimuisewon <todimuisewon@gmail.com>
2023-07-01 10:42:15 +0100

    refactored code

--- src/main/java/io/todimu/practice/studentportal/annotation/RateLimited.java
+++ src/main/java/io/todimu/practice/studentportal/annotation/RateLimited.java
@@ -11,3 +11,11 @@ public @interface RateLimited {
 
 }
 
+// method limiting -> need to add Allow header to the responses
+// security policies -> have a Content-Security-Policy header defined
+// strict-transport-security -> strict-transport-security header in your API responses
+// Content Type Specification -> Accept header exists and its value is set to application/json.
+// Rate Limiting -> x-ratelimit-* headers in the response payload to see if you correctly use rate limiting
+// Secure Connection -> Treblle automatically detects whether or not the requests your end customers are making are using HTTPs instead of HTTP.
+// IP reputation -> own network as well as 3rd party services to check the reputation of your users IP address
+// SQL injection
